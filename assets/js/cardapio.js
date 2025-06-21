53813c740ee5817d74a89adc3f294027358d9ec7
Bruno Henrique <br.henriquep03@gmail.com>
2025-06-21 16:00:40 -0300

    Adicionado o tooltip e menu do cardápio

---
+++ assets/js/cardapio.js
@@ -0,0 +1,112 @@
+const cardapio = [
+  // CAFÉS
+  {
+    imagem: './assets/img/xicara-cafe.jpg',
+    nome: 'Cappuccino Clássico',
+    descricao: 'Espresso, leite vaporizado e espuma cremosa.',
+    valor: 12.90,
+    categoria: 'cafe'
+  },
+  {
+    imagem: './assets/img/xicara-cafe.jpg',
+    nome: 'Mocha Caramelizado',
+    descricao: 'Chocolate, café e calda de caramelo.',
+    valor: 15.50,
+    categoria: 'cafe'
+  },
+  {
+    imagem: './assets/img/xicara-cafe.jpg',
+    nome: 'Cold Brew com Tônico',
+    descricao: 'Café gelado fermentado + água tônica.',
+    valor: 18.00,
+    categoria: 'cafe'
+  },
+  {
+    imagem: './assets/img/xicara-cafe.jpg',
+    nome: 'Latte de Baunilha',
+    descricao: 'Espresso suave com essência de baunilha.',
+    valor: 14.00,
+    categoria: 'cafe'
+  },
+  {
+    imagem: './assets/img/xicara-cafe.jpg',
+    nome: 'Aeropress',
+    descricao: 'Método de extração intenso e aromático.',
+    valor: 16.00,
+    categoria: 'cafe'
+  },
+
+  // SOBREMESAS
+  {
+    imagem: './assets/img/sobremesa.jpg',
+    nome: 'Cheesecake de Doce de Leite',
+    descricao: 'Creme suave com base de biscoito.',
+    valor: 22.00,
+    categoria: 'sobremesa'
+  },
+  {
+    imagem: './assets/img/sobremesa.jpg',
+    nome: 'Brigadeiro Gourmet',
+    descricao: '4 unidades: café, ninho e nutella.',
+    valor: 14.50,
+    categoria: 'sobremesa'
+  },
+  {
+    imagem: './assets/img/sobremesa.jpg',
+    nome: 'Torta de Maçã Canela',
+    descricao: 'Massa amanteigada e recheio caramelizado.',
+    valor: 18.90,
+    categoria: 'sobremesa'
+  },
+  {
+    imagem: './assets/img/sobremesa.jpg',
+    nome: 'Cookie de Chocolate Belga',
+    descricao: 'Crocante por fora, derretido por dentro.',
+    valor: 9.90,
+    categoria: 'sobremesa'
+  },
+  {
+    imagem: './assets/img/sobremesa.jpg',
+    nome: 'Croissant de Amêndoas',
+    descricao: 'Folhado crocante com recheio frangipane.',
+    valor: 12.00,
+    categoria: 'sobremesa'
+  },
+
+  // LIVROS
+  {
+    imagem: './assets/img/livro.jpg',
+    nome: 'A Guerra dos Tronos',
+    descricao: 'Livro 1 da série Game of Thrones.',
+    valor: 59.90,
+    categoria: 'livro'
+  },
+  {
+    imagem: './assets/img/livro.jpg',
+    nome: 'Cidade dos Ossos',
+    descricao: 'Série Os Instrumentos Mortais.',
+    valor: 45.00,
+    categoria: 'livro'
+  },
+  {
+    imagem: './assets/img/livro.jpg',
+    nome: 'O Nome do Vento',
+    descricao: 'Fantasia épica sobre música e magia.',
+    valor: 49.90,
+    categoria: 'livro'
+  },
+  {
+    imagem: './assets/img/livro.jpg',
+    nome: '1984',
+    descricao: 'Distopia clássica de George Orwell.',
+    valor: 39.90,
+    categoria: 'livro'
+  },
+  {
+    imagem: './assets/img/livro.jpg',
+    nome: 'Cem Anos de Solidão',
+    descricao: 'Realismo mágico de García Márquez.',
+    valor: 55.00,
+    categoria: 'livro'
+  }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,81 @@
-const swiper = new Swiper(".mySwiper", {
-  effect: "flip",
-  grabCursor: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//tooltip dos ícones da seção sobre
+function mostrarTooltip(e){
+  const elementoIcone = e.target;
+  const elementoDescricao = elementoIcone.nextElementSibling;
+  if (elementoDescricao){
+    elementoDescricao.classList.add('visivel');
+  }
+
+};
+
+function esconderTooltip(e){
+  const elementoIcone = e.target;
+  const elementoDescricao = elementoIcone.nextElementSibling;
+  if (elementoDescricao){
+    elementoDescricao.classList.remove('visivel');
+  }  
+}
+
+document.querySelectorAll( '.tooltip-sobre' ).forEach((element)=>{
+  element.addEventListener('mouseenter', mostrarTooltip);
+  element.addEventListener('mouseleave', esconderTooltip);
+});
+
+//abas do cardápio
+function selecionarAbaCardapio(e){
+  document.querySelectorAll( '.menu-cardapio ul li' ).forEach((element)=>{
+    if (element.classList.contains('ativo')){
+      element.classList.remove('ativo');
+    }    
+  });
+
+  e.target.classList.add('ativo');
+
+  // listar os itens  
+
+  const containerCardapio = document.querySelector('.itens-cardapio');
+  const categoria = e.target.dataset.categoria;
+  const itensCategoria = cardapio.filter( (item)=>{
+    if (item.categoria === categoria) return true;
+  });  
+
+  let cards = '';
+  for (let i = 0; i<= itensCategoria.length - 1; i++){
+    let card = '<div class="card-item-cardapio">' +
+                 '<img src="'+ itensCategoria[i].imagem +'">' +                                
+                   '<div class="card-item-info">' +
+                     '<h3>'+ itensCategoria[i].nome +'</h3>'+
+                     '<span class="descricao-item">'+ itensCategoria[i].descricao+ '</span>'+                                
+                     '<span class="valor-item">'+ itensCategoria[i].valor.toLocaleString('pt-BR', { 
+                                                    style: 'currency', 
+                                                    currency: 'BRL' 
+                                                  }) +'</span>' +
+                     '<button class="btn">Mais informações</button>'+
+                   '</div>' +                            
+                '</div>';
+
+    cards += card;              
+  };
+
+  containerCardapio.innerHTML = cards;
+
+  
+}
+
+document.querySelectorAll('.menu-cardapio ul li').forEach((element)=>{
+  element.addEventListener('click', selecionarAbaCardapio)
+});
+
+
+//SLIDER DA ÁREA DE EVENTOS
+function trocarSlider(e){
+  const index = e.target.dataset.index;
+  console.log(index)
+
+  const larguraSlider = document.querySelector('.slider-item').scrollWidth;
+  document.querySelector( '.slider-area' ).style.left = '-' + ((index - 1) * larguraSlider) + 'px';
+}
+
+document.querySelectorAll('.slider-controle').forEach((element)=>{
+  element.addEventListener('click', trocarSlider)
 });
